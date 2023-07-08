6ec36f9693d22e933c7df298bd376601f54bd9e7
Ivana Tanzil <ivanatanzil17@gmail.com>
2023-07-08 19:57:15 +0700

    beginning

---
+++ src/books.js
@@ -0,0 +1,17 @@
+export const books = [
+  { id:1,
+    img: 'https://m.media-amazon.com/images/I/71F4+7rk2eL._AC_UL400_.jpg',
+    title: 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    author: 'James Clear and Penguin Audio'
+  },
+  { id:2,
+    img: 'https://m.media-amazon.com/images/I/81rTDFiANLL._AC_UL400_.jpg',
+    title: 'Devoted',
+    author: 'Dean Koontz'
+  },
+  { id:3,
+    img: 'https://m.media-amazon.com/images/I/71oWaTEjEkL._AC_UL400_.jpg',
+    title: 'Happy Place',
+    author: 'Emily Henry'
+  }
+];
